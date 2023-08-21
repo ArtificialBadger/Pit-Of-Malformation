69208522c4eb765b7400f4d1df3ce2ef5925e58c
Isaac Zais <isaaczais@hotmail.com>
2023-08-21 15:02:24 -0700

    Add Goals

--- Avotion/Server/Controllers/WeatherForecastController.cs
+++ Avotion/Server/Controllers/WeatherForecastController.cs
@@ -18,15 +18,15 @@ public class WeatherForecastController : ControllerBase
 		_logger = logger;
 	}
 
-	[HttpGet]
-	public IEnumerable<WeatherForecast> Get()
-	{
-		return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-		{
-			Date = DateTime.Now.AddDays(index),
-			TemperatureC = Random.Shared.Next(-20, 55),
-			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-		})
-		.ToArray();
-	}
+	//[HttpGet]
+	//public IEnumerable<WeatherForecast> Get()
+	//{
+	//	return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+	//	{
+	//		Date = DateTime.Now.AddDays(index),
+	//		TemperatureC = Random.Shared.Next(-20, 55),
+	//		Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+	//	})
+	//	.ToArray();
+	//}
 }
