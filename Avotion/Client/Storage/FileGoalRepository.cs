b5c5c221c058b723a03110f3ca08893fd535445c
Isaac Zais <isaaczais@hotmail.com>
2023-09-12 11:52:30 -0700

    wip

--- Avotion/Client/Storage/FileGoalRepository.cs
+++ Avotion/Client/Storage/FileGoalRepository.cs
@@ -1,12 +1,11 @@
 ﻿using Avotion.Shared.Goals;
 using Microsoft.Extensions.FileProviders;
+using System.Diagnostics;
 
 namespace Avotion.Client.Storage;
 
 public class FileGoalRepository : IGoalRepository
 {
-    private string Path = "goals.json";
-
     public Task SaveDay(Day day)
     {
         throw new NotImplementedException();
@@ -14,7 +13,10 @@ public class FileGoalRepository : IGoalRepository
 
     public async Task SaveGoals(IEnumerable<Goal> Goals)
     {
-        using var file = File.Create("./avotion.test");
+        var parentDir = Directory.GetParent(".");
+        using var file = File.Create("../.././avotion.test");
+        Debug.WriteLine(file.Name);
+        Debug.WriteLine(parentDir);
         await Task.Delay(100);
     }
 }
